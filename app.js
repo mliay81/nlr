84725ed801fc0a854778ed6a93d729b22da75577
mliay81 <mikeliay@gmail.com>
2019-03-26 23:08:21 -0500

    bulk of design done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var app = express()
 const port = 3000
+app.set("view engine", "ejs")
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 

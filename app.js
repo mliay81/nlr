9dbd0043c65f00e022f7ef5d1e25e09aa0a14389
mliay81 <mikeliay@gmail.com>
2019-03-31 15:33:41 -0500

    routing issue

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var express = require("express")
 var app = express()
 const port = 3000
-app.set("view engine", "ejs")
+var http = require("http")
+const path = require('path')
+// app.set("view engine", "html")
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/sponsors', function(req, res) {
-    res.render('/sponsors')
-  })
+// New code based on Stack Overflow that didn't fix routing.
+// app.use('/', express.static(__dirname + '/public'));
+
+// app.get('/public/sponsors.html', function(req, res) {
+//     res.sendFile('sponsors')
+//   })
 
   app.get('/', function(req, res) {
-    res.render('index')
+    res.sendFile('index')
   })
 
   app.get('/team', function(req, res) {
-    res.render('/team')
+    res.sendFile('team')
   })

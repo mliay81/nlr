e30fa7373f0c0157fd724d49219058b3839fae82
mliay81 <mikeliay@gmail.com>
2019-04-07 22:40:43 -0500

    gh pages

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + '/public'));
 //   })
 
   app.get('/team', function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/team.html"))
+    res.sendFile(path.join(__dirname, "/public/team.html"))
   })
 
   

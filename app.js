8d33105976a3c4d0477ced1d91f0d6ee4f31bef8
mliay81 <mikeliay@gmail.com>
2019-04-10 20:34:36 -0500

    design changes

--- app.js
+++ app.js
@@ -34,3 +34,7 @@ app.use(express.static(__dirname + '/public'));
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"))
   })
+
+  app.get('/calendar', function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/calendar.html"))
+  })
